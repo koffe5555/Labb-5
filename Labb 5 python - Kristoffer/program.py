7d53df7a9b5edea0574ff58714bb0526714becd1
koffe5555 <56026120+koffe5555@users.noreply.github.com>
2020-03-05 00:31:25 +0100

    Add files via upload

--- Labb 5 python - Kristoffer/program.py
+++ Labb 5 python - Kristoffer/program.py
@@ -23,22 +23,27 @@ man_df.set_index('år')
 #Summan av dataframen
 totala = man_df[['2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019']]
 p = totala.sum()
+print('\n')
 print(f'Totala mängden dödsfall för varje år:\n {p}')
 
+#år_hist = man_df[['2019']]
+
 #Hsitogram
-#plt.figure(figsize=(2, 4))
-totala.plot(kind='hist')
-#plt.hist(år_man)
-plt.xlabel('Dödsfall')
-plt.ylabel('ålder')
+#plt.figure(figsize=(8, 6))
+totala.hist(grid=False, figsize=(8, 6))
+plt.xlabel('Ålder')
+plt.ylabel('Dödsfall')
 #plt.tight_layout
 plt.show()
 
+
+graf = man_df[['2002', '2012', '2013', '2000', '2019']]
+
 #GRAF
-# år_man.plot(title='Ålder på dödsfall', kind='line')
-# plt.ylabel('Dödsfall')
-# plt.xlabel('Ålder')
-# plt.show()
+graf.plot(title='Ålder på dödsfall', kind='line')
+plt.ylabel('Dödsfall')
+plt.xlabel('Ålder')
+plt.show()
 
 #SCATTER 2
 # plt.scatter(man_df['år'], man_df['2019'], label="Point", color='r', marker='o', s=10)
@@ -49,28 +54,33 @@ plt.show()
 # plt.legend()
 # plt.show()
 
+marker = '2000', '2009', '2019'
 
 #SCATTER
-# fig, scat = plt.subplots(figsize=(16, 8))
-# scat.scatter(man_df['år'], man_df['2000'], man_df['2009'], man_df['2019'])
-# scat.set_xlabel('Ålder')
-# scat.set_ylabel('dödsfall')
-# plt.show()
+fig, scat = plt.subplots(figsize=(8, 4))
+scat.scatter(man_df['år'], man_df['2000'])
+scat.scatter(man_df['år'], man_df['2009'])
+scat.scatter(man_df['år'], man_df['2019'])
+scat.set_xlabel('Ålder')
+scat.set_ylabel('dödsfall')
+plt.legend(marker)
+plt.show()
 
 #HeatMaps
-# correlation_matrix = man_df.corr().round(2)
-# sns.heatmap(data=correlation_matrix, annot=True)
-# plt.show()
+correlation_matrix = man_df.corr()
+sns.heatmap(data=correlation_matrix, annot=True)
+plt.show()
+
 
 #Ta bort NULL
 #df_csv = df_csv.dropna()
 
 
 #RÄKNA MEDIAN
-# median = man_df['2000']
-# procent = percentile(median, 25, 50, 75)
-# print(f"medianen för åren är : ",(procent))
-# low_count = percentile(median, 1)
-# high_count = percentile(median, 100)
-# print(f"Lägsta dödsantalet", low_count, "st")
-# print(f"Högsta dödsantalet", high_count, "st")
+median = man_df['2002']
+procent = percentile(median, (25, 50, 75))
+print(f"medianen är : ",(procent))
+low_count = percentile(median, 1)
+high_count = percentile(median, 100)
+print(f"Lägsta dödsantalet", low_count, "st")
+print(f"Högsta dödsantalet", high_count, "st")
